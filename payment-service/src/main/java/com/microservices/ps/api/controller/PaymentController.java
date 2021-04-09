a8ef4394fbe2d1dfe7e95d4d5c68b91bcad5797b
Lendrit <lendritshala3@gmail.com>
2021-04-10 00:52:22 +0200

    add route through API Gateway

--- payment-service/src/main/java/com/microservices/ps/api/controller/PaymentController.java
+++ payment-service/src/main/java/com/microservices/ps/api/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.microservices.ps.api.entity.Payment;
 import com.microservices.ps.api.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
@@ -19,4 +16,10 @@ public class PaymentController {
     public Payment doPayment(@RequestBody Payment payment){
         return service.doPayment(payment);
     }
+
+    @GetMapping("/{orderId}")
+    public Payment findPaymentHistoryByOrderId(@PathVariable int orderId){
+        return service.findPaymentHistoryByOrderId(orderId);
+    }
+
 }
