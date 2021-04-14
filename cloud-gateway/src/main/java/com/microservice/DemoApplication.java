10b4f0e65ec073c1fc82860b97ff32d86a4255a7
Lendrit <lendritshala3@gmail.com>
2021-04-14 10:24:31 +0200

    Cloud-Gateway change version spring boot and Hystrix configuration

--- cloud-gateway/src/main/java/com/microservice/DemoApplication.java
+++ cloud-gateway/src/main/java/com/microservice/DemoApplication.java
@@ -1,11 +1,19 @@
 package com.microservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+/*
+	Hystrix serves when a microservice is stopped by returning a message as a notification to that passive server
+ */
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
