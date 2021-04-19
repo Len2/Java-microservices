f596069604ef74d4c56b6ff32ad31fa1be6cd40a
Lendrit <lendritshala3@gmail.com>
2021-04-19 16:55:41 +0200

    Hystrix Dashboard

--- cloud-gateway/src/main/java/com/microservice/DemoApplication.java
+++ cloud-gateway/src/main/java/com/microservice/DemoApplication.java
@@ -1,10 +1,7 @@
 package com.microservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
